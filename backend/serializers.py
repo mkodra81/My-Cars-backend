5d8d5e3b049b36a7d1597fff87d832ce059468ed
Mateo <kodramateo4@gmail.com>
2025-11-21 14:35:20 +0100

    fixed date_joined return

--- backend/serializers.py
+++ backend/serializers.py
@@ -17,7 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'first_name', 'last_name',]
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'date_joined']
 
 
     def create(self, validated_data):
